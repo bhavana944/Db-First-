8f07b69faf63632859873b583998dca73b88624a
eerpinat <eerpinat@protrans.com>
2023-05-29 12:27:40 +0530

    Add project files.

---
+++ Db(First)/Controllers/StudentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Db_First_.Models;
+
+namespace Db_First_.Controllers
+{
+    public class StudentController : Controller
+    {
+        BhavanaEntities1 bhavanaEntities1 = new BhavanaEntities1();
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Db(First)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Db_First_.MvcApplication" Language="C#" %>
